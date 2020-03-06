93e2686b1df1b788d48cdfbd0e8920ae2849b6f7
dave-yang <daworld.ny@gmail.com>
2020-03-06 22:43:52 +0500

    Fixes in route group

--- Config/routes.php
+++ Config/routes.php
@@ -11,18 +11,18 @@
 
 return [
     '%s/module/insurance/issuer' => [
-        'controller' => 'Insurance:Issuer@indexAction'
+        'controller' => 'Admin:Issuer@indexAction'
     ],
 
     '%s/module/insurance/issuer/add' => [
-        'controller' => 'Insurance:Issuer@addAction'
+        'controller' => 'Admin:Issuer@addAction'
     ],
 
     '%s/module/insurance/issuer/edit/(:var)' => [
-        'controller' => 'Insurance:Issuer@editAction'
+        'controller' => 'Admin:Issuer@editAction'
     ],
 
     '%s/module/insurance/issuer/delete/(:var)' => [
-        'controller' => 'Insurance:Issuer@deleteAction'
+        'controller' => 'Admin:Issuer@deleteAction'
     ]
 ];
