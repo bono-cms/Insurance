73d218b06ba6772e8f1dfb43fbba9cae2b316bb6
dave-yang <daworld.ny@gmail.com>
2020-03-06 23:34:10 +0500

    Issuer clients schema and its mapper

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -8,3 +8,16 @@ CREATE TABLE bono_module_insurance_issuer (
     `email` varchar(255) NOT NULL,
     `created_at` DATETIME NOT NULL
 );
+
+/* Clients of issuers */
+DROP TABLE IF EXISTS bono_module_insurance_issuer_clients;
+CREATE TABLE bono_module_insurance_issuer_clients (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `issuer_id` INT NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `passport` varchar(255) NOT NULL,
+    `birthday` DATE NOT NULL,
+
+    FOREIGN KEY (issuer_id) REFERENCES bono_module_insurance_issuer(id) ON DELETE CASCADE
+
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
